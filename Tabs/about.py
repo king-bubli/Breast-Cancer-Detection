f543473e4602fd5d15c577e126fd3bdab5736e2e
Srijata Chanda <129683845+bubli-c@users.noreply.github.com>
2024-07-16 21:19:06 +0530

    Create about.py

---
+++ Tabs/about.py
@@ -0,0 +1,27 @@
+"""This modules contains data about about page"""
+
+# Import necessary modules
+import streamlit as st
+from PIL import Image
+
+
+def app():
+    """This function creates the about page"""
+    st.balloons()
+    st.title('Contact Me')
+    st.markdown("""# ***About the Developer***
+
+---
+
+## **Name:** *Srijata Chanda*
+                
+                             
+### **Bio**
+As a senior-year student pursuing my B.Tech degree in Artificial Intelligence and Machine Learning at Narula Institute of Technology, I’m deeply passionate about creating innovative solutions in the AI field. Looking ahead, I’m excited to contribute to cutting-edge developments and explore new frontiers. If you share this enthusiasm or need any assistance, feel free to connect - I’m always up for an AI chat! 😊
+
+## **LinkedIn:** [srijata-chanda](https://www.linkedin.com/in/srijata-chanda/)
+
+## **Github:** [bubli-c](https://github.com/bubli-c)
+
+## **Email:** [srijatachanda195@gmail.com](https://mail.google.com/mail/u/0/?fs=1&to=srijatachanda195@gmail.com.com&tf=cm)""")
+    
